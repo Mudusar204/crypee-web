23b2fcddffdeefbabb5c5f9f6b640c74d58a9c6a
Usmanwasim <mianusmanwasim@gmail.com>
2024-03-11 13:21:28 +0500

    update crypto pricess table with pagination

--- src/Pages/CryptoPrices/CryptoPrices.jsx
+++ src/Pages/CryptoPrices/CryptoPrices.jsx
@@ -1,185 +1,56 @@
+import { useEffect, useState } from 'react';
 import { Box, Container } from '@mui/material';
+
+import axios from 'axios';
 
 import Navigation from '../../Components/Navigation';
 import SearchSection from './SearchSection';
 import CoinsPrices from './CoinsPrices';
-import tetherImg from '../../images/coins/tether_img.png';
-import bitcoinImg from '../../images/coins/bitcoin_img.png';
-import etherImg from '../../images/coins/ether_img.png';
-import usdcImg from '../../images/coins/usdc_img.png';
-import busdImg from '../../images/coins/busd_img.png';
-import wethImg from '../../images/coins/weth_img.png';
-import daiImg from '../../images/coins/dai_img.png';
-import xrpImg from '../../images/coins/xrp_img.png';
-import litecoinImg from '../../images/coins/litecoin_img.png';
-import solanaImg from '../../images/coins/solana_img.png';
-import froggiesImg from '../../images/coins/froggies_img.png';
-import maticImg from '../../images/coins/matic_img.png';
-import bnbImg from '../../images/coins/bnb_img.png';
 import CoinTable from './Cryptohistory';
-
-const historyDetails = [
-    {
-        coin: { short: 'USDT', coinName: 'Tether', icon: tetherImg },
-        price: { currency: 'PKR', value: '212.89' },
-        oneDayChange: '-0.48',
-        marketCap: '8.1T',
-        oneDayVolume: '15.9T',
-        supply: '79.5B',
-    },
-    {
-        coin: { short: 'BTC', coinName: 'Bitcoin', icon: bitcoinImg },
-        price: { currency: 'PKR', value: '336.91' },
-        oneDayChange: '2.82',
-        marketCap: '5.5T',
-        oneDayVolume: '14.7T',
-        supply: '57.2B',
-    },
-    {
-        coin: { short: 'ETH', coinName: 'Ether', icon: etherImg },
-        price: { currency: 'PKR', value: '117.23' },
-        oneDayChange: '-1.1',
-        marketCap: '14.2T',
-        oneDayVolume: '18.5T',
-        supply: '97.9B',
-    },
-    {
-        coin: { short: 'USDC', coinName: 'USD Coin', icon: usdcImg },
-        price: { currency: 'PKR', value: '221.51' },
-        oneDayChange: '-1.2',
-        marketCap: '6.9T',
-        oneDayVolume: '17.7T',
-        supply: '76.6B',
-    },
-    {
-        coin: { short: 'BUSD', coinName: 'Binance USD', icon: busdImg },
-        price: { currency: 'PKR', value: '167.64' },
-        oneDayChange: '0.42',
-        marketCap: '28.0T',
-        oneDayVolume: '9.9T',
-        supply: '79.1B',
-    },
-    {
-        coin: { short: 'WETH', coinName: 'Weth', icon: wethImg },
-        price: { currency: 'PKR', value: '353.79' },
-        oneDayChange: '0.11',
-        marketCap: '10.8T',
-        oneDayVolume: '12.2T',
-        supply: '79.6B',
-    },
-    {
-        coin: { short: 'DAI', coinName: 'Dai', icon: daiImg },
-        price: { currency: 'PKR', value: '156.12' },
-        oneDayChange: '-1.6',
-        marketCap: '21.5T',
-        oneDayVolume: '10.0T',
-        supply: '72.2B',
-    },
-    {
-        coin: { short: 'XRP', coinName: 'XRP', icon: xrpImg },
-        price: { currency: 'PKR', value: '310.04' },
-        oneDayChange: '-1.6',
-        marketCap: '20.7T',
-        oneDayVolume: '17.8T',
-        supply: '73.9B',
-    },
-    {
-        coin: { short: 'LTC', coinName: 'Litecoin', icon: litecoinImg },
-        price: { currency: 'PKR', value: '276.27' },
-        oneDayChange: '1.16',
-        marketCap: '8.0T',
-        oneDayVolume: '18.6T',
-        supply: '64.7B',
-    },
-    {
-        coin: { short: 'SOL', coinName: 'Solana', icon: solanaImg },
-        price: { currency: 'PKR', value: '272.09' },
-        oneDayChange: '-1.0',
-        marketCap: '18.9T',
-        oneDayVolume: '11.6T',
-        supply: '87.0B',
-    },
-    {
-        coin: { short: 'FRGST', coinName: 'Froggies Token', icon: froggiesImg },
-        price: { currency: 'PKR', value: '274.91' },
-        oneDayChange: '0.67',
-        marketCap: '17.4T',
-        oneDayVolume: '10.0T',
-        supply: '74.8B',
-    },
-    {
-        coin: { short: 'MATIC', coinName: 'Polygon', icon: maticImg },
-        price: { currency: 'PKR', value: '311.41' },
-        oneDayChange: '-0.8',
-        marketCap: '6.7T',
-        oneDayVolume: '13.8T',
-        supply: '79.3B',
-    },
-    {
-        coin: { short: 'BNB', coinName: 'Binance Coin', icon: bnbImg },
-        price: { currency: 'PKR', value: '344.84' },
-        oneDayChange: '1.18',
-        marketCap: '17.1T',
-        oneDayVolume: '5.8T',
-        supply: '81.0B',
-    },
-    {
-        coin: { short: 'XRP', coinName: 'XRP', icon: xrpImg },
-        price: { currency: 'PKR', value: '310.04' },
-        oneDayChange: '-1.6',
-        marketCap: '20.7T',
-        oneDayVolume: '17.8T',
-        supply: '73.9B',
-    },
-    {
-        coin: { short: 'LTC', coinName: 'Litecoin', icon: litecoinImg },
-        price: { currency: 'PKR', value: '276.27' },
-        oneDayChange: '1.16',
-        marketCap: '8.0T',
-        oneDayVolume: '18.6T',
-        supply: '64.7B',
-    },
-    {
-        coin: { short: 'SOL', coinName: 'Solana', icon: solanaImg },
-        price: { currency: 'PKR', value: '272.09' },
-        oneDayChange: '-1.0',
-        marketCap: '18.9T',
-        oneDayVolume: '11.6T',
-        supply: '87.0B',
-    },
-    {
-        coin: { short: 'FRGST', coinName: 'Froggies Token', icon: froggiesImg },
-        price: { currency: 'PKR', value: '274.91' },
-        oneDayChange: '0.67',
-        marketCap: '17.4T',
-        oneDayVolume: '10.0T',
-        supply: '74.8B',
-    },
-    {
-        coin: { short: 'MATIC', coinName: 'Polygon', icon: maticImg },
-        price: { currency: 'PKR', value: '311.41' },
-        oneDayChange: '-0.8',
-        marketCap: '6.7T',
-        oneDayVolume: '13.8T',
-        supply: '79.3B',
-    },
-    {
-        coin: { short: 'BNB', coinName: 'Binance Coin', icon: bnbImg },
-        price: { currency: 'PKR', value: '344.84' },
-        oneDayChange: '1.18',
-        marketCap: '17.1T',
-        oneDayVolume: '5.8T',
-        supply: '81.0B',
-    },
-];
+import { REACT_APP_BASE_URL } from '../../config';
 
 const CryptoPrices = () => {
+    const [cryptoPricesData, setCryptoPricesData] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [activePage, setActivePage] = useState(1);
+    const [pageLimit, setPageLimit] = useState(100);
+    useEffect(() => {
+        const fetchCryptos = async () => {
+            try {
+                const refreshToken = localStorage.getItem('persistMe')
+                    ? JSON.parse(localStorage.getItem('persistMe'))
+                    : null;
+                // console.log(activePage, pageLimit);
+                let response = await axios.get(`${REACT_APP_BASE_URL}/api/data/getCryptoPrices`, {
+                    params: { page: activePage, limit: pageLimit },
+                    headers: {
+                        Authorization: refreshToken?.user?.token,
+                    },
+                });
+
+                // console.log(response.data?.data, 'response.data-=-=-');
+                setCryptoPricesData(response.data?.data?.data);
+                setPageCount(response.data?.data?.pageCount);
+                setActivePage(response.data?.data?.currentPage);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchCryptos();
+    }, [activePage, pageLimit]);
     return (
-        <Box  mx={{lg:7,xs:2,md:4,sm:3}}>
-            <Navigation/>
-                    <SearchSection />
-                    <CoinsPrices />
-                    <CoinTable data={historyDetails} />
+        <Box mx={{ lg: 7, xs: 2, md: 4, sm: 3 }}>
+            <Navigation />
+            <SearchSection />
+            <CoinsPrices />
+            <CoinTable
+                data={cryptoPricesData}
+                pageCount={pageCount}
+                activePage={activePage}
+                setActivePage={setActivePage}
+                pageLimit={pageLimit}
+                setPageLimit={setPageLimit}
+            />
         </Box>
     );
 };
--- src/Pages/CryptoPrices/Cryptohistory.jsx
+++ src/Pages/CryptoPrices/Cryptohistory.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Box, styled } from '@mui/material';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Typography,
+    Box,
+    styled,
+    Skeleton,
+} from '@mui/material';
 import Pagination from '@mui/material/Pagination';
 
-const CoinTable = ({ data }) => {
-    const columns = ['Coin', 'Price (PKR)', '1-Day Change', 'Market Cap', '1-Day Volume', 'Supply'];
-
-    const [page, setPage] = useState(1);
-    const rowsPerPage = 13;
+const CoinTable = ({ data, pageCount, activePage, setActivePage, pageLimit, setPageLimit }) => {
+    const columns = [
+        'Coin',
+        'Price (USD)',
+        '1-Day %',
+        'Market Cap',
+        '1-Day Volume',
+        'Circulating Supply',
+    ];
 
     const PaginationWrapper = styled(Box)({
         display: 'flex',
@@ -14,7 +30,7 @@ const CoinTable = ({ data }) => {
         mt: 2,
         '& .Mui-selected': {
             background: 'linear-gradient(180deg, #0B7BC4 0%, #5BACDE 100%)',
-            color: '#fff',
+            color: '#fff !important',
         },
         '& button': {
             display: 'flex',
@@ -34,73 +50,148 @@ const CoinTable = ({ data }) => {
     });
 
     const handleChangePage = (event, newPage) => {
-        setPage(newPage);
+        setActivePage(newPage);
     };
 
-    const itemsPerPage = 5;
-    const startIndex = (page - 1) * rowsPerPage;
-    const endIndex = startIndex + rowsPerPage;
-    const displayedHistoryDetails = data.slice(startIndex, endIndex);
-    const totalRows = data.length;
-    const totalPages = Math.ceil(totalRows / itemsPerPage);
+    // format number value to 1K, 1M, 1B etc
+    const formatNumberValue = (value) => {
+        const absValue = Math.abs(value);
+        if (absValue < 1000) {
+            return value;
+        } else if (absValue >= 1e12) {
+            // Trillion
+            const billions = (value / 1e12).toFixed(1);
+            return billions.endsWith('.0') ? billions.slice(0, -2) + 'T' : billions + 'T';
+        } else if (absValue >= 1e9) {
+            // Billion
+            const billions = (value / 1e9).toFixed(1);
+            return billions.endsWith('.0') ? billions.slice(0, -2) + 'B' : billions + 'B';
+        } else if (absValue >= 1e6) {
+            // Million
+            const millions = (value / 1e6).toFixed(1);
+            return millions.endsWith('.0') ? millions.slice(0, -2) + 'M' : millions + 'M';
+        } else if (absValue >= 1e3) {
+            // Thousand
+            const thousands = (value / 1e3).toFixed(0);
+            return thousands.endsWith('.0') ? thousands.slice(0, -2) + 'K' : thousands + 'K';
+        }
+    };
 
     return (
         <Box>
-            <TableContainer component={Paper} sx={{ mt: 5 }}>
-                <Table>
-                    <TableHead
-                        sx={{
-                            background:
-                                'linear-gradient(180deg, #29A3F1 -27.01%, #2896DC 36.23%, #0B7AC1 110.95%)',
-                            boxShadow: ' 0px 5px 20px 0px #0B7BC3 inset',
-                        }}
-                    >
+            <TableContainer component={Paper} sx={{ mt: 5, maxHeight: '90vh', minWidth: '500px' }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
                         <TableRow>
-                            {columns.map((column) => (
-                                <TableCell sx={{ color: 'white' }} key={column}>
+                            {columns.map((column, i) => (
+                                <TableCell
+                                    sx={{
+                                        color: 'white',
+                                        textAlign: i === columns.length - 1 ? 'right' : 'left',
+
+                                        background:
+                                            'linear-gradient(180deg, #29A3F1 -27.01%, #2896DC 36.23%, #0B7AC1 110.95%) !important',
+                                    }}
+                                    key={column}
+                                >
                                     {column}
                                 </TableCell>
                             ))}
                         </TableRow>
                     </TableHead>
-
                     <TableBody>
-                        {displayedHistoryDetails.map((row, index) => (
-                            <TableRow key={index}>
-                            <TableCell>
-                                <Box display="flex" gap={'10px'}>
-                                    <img
-                                        src={row.coin.icon}
-                                        alt={row.coin.short}
-                                        style={{ width: '30px', marginRight: '5px' }}
-                                    />
-                    
-                                    <Typography>
-                                        {row.coin.short}
-                                        <Typography fontSize={'10px'} fontWeight={500}>
-                                            {row.coin.coinName}
-                                        </Typography>
-                                    </Typography>
-                                </Box>
-                            </TableCell>
-                            <TableCell>{`${row.price.currency} ${row.price.value}`}</TableCell>
-                            <TableCell>{`${row.oneDayChange}%`}</TableCell>
-                            <TableCell>{row.marketCap}</TableCell>
-                            <TableCell>{row.oneDayVolume}</TableCell>
-                            <TableCell>{row.supply}</TableCell>
-                        </TableRow>
-                        ))}
+                        {data?.length > 0
+                            ? data?.map((row, i) => (
+                                  <TableRow key={i}>
+                                      <TableCell>
+                                          <Box display="flex" gap={'10px'}>
+                                              <img
+                                                  width={35}
+                                                  height={35}
+                                                  src={`https://s2.coinmarketcap.com/static/img/coins/32x32/${row?.id}.png`}
+                                                  alt={row?.name}
+                                              />
+
+                                              <Typography>
+                                                  {row?.name}
+                                                  <Typography fontSize={'10px'} fontWeight={500}>
+                                                      {row?.symbol}
+                                                  </Typography>
+                                              </Typography>
+                                          </Box>
+                                      </TableCell>
+                                      <TableCell>{`USD ${(+row?.quote?.USD?.price).toFixed(
+                                          4,
+                                      )}`}</TableCell>
+                                      <TableCell>{`${(+row?.quote?.USD?.percent_change_24h).toFixed(
+                                          2,
+                                      )}%`}</TableCell>
+                                      <TableCell>{`USD ${formatNumberValue(
+                                          +row?.quote?.USD?.market_cap,
+                                      )}`}</TableCell>
+                                      <TableCell>{`USD ${formatNumberValue(
+                                          +row?.quote?.USD?.volume_24h,
+                                      )}`}</TableCell>
+                                      <TableCell sx={{ textAlign: 'right' }}>
+                                          {row?.circulating_supply}
+                                      </TableCell>
+                                  </TableRow>
+                              ))
+                            : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((row, i) => (
+                                  <TableRow key={i}>
+                                      <TableCell
+                                          sx={{
+                                              display: 'flex',
+                                              gap: '10px',
+                                              alignItems: 'center',
+                                          }}
+                                      >
+                                          <Skeleton
+                                              variant="circular"
+                                              sx={{
+                                                  width: { xs: '35px', sm: '40px' },
+                                                  height: { xs: '35px', sm: '40px' },
+                                              }}
+                                          />
+                                          <Box>
+                                              <Skeleton sx={{ width: '70px' }} />
+                                              <Skeleton sx={{ width: '40px' }} />
+                                          </Box>
+                                      </TableCell>
+                                      <TableCell>
+                                          <Skeleton sx={{ width: '100px' }} />
+                                      </TableCell>
+                                      <TableCell>
+                                          <Skeleton sx={{ width: '40px' }} />
+                                      </TableCell>
+                                      <TableCell>
+                                          <Skeleton sx={{ width: '70px' }} />
+                                      </TableCell>
+                                      <TableCell>
+                                          <Skeleton sx={{ width: '90px' }} />
+                                      </TableCell>
+                                      <TableCell>
+                                          <Skeleton sx={{ width: '120px', ml: 'auto' }} />
+                                      </TableCell>
+                                  </TableRow>
+                              ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-            <PaginationWrapper sx={{ '& button': { width: '50px', padding: '1.6em 0 1.8em 0' } }}>
+            <PaginationWrapper
+                sx={{
+                    '& button': {
+                        width: { xs: '35px', sm: '50px' },
+                        height: { xs: '35px', sm: '50px' },
+                    },
+                }}
+            >
                 <Pagination
-                    color="primary"
-                    count={totalPages}
-                    defaultPage={1}
-                    page={page}
+                    count={pageCount}
+                    defaultPage={+activePage}
+                    page={+activePage}
                     onChange={handleChangePage}
-                    labelRowsPerPage=""
+                    boundaryCount={1}
                 />
             </PaginationWrapper>
         </Box>
@@ -108,14 +199,3 @@ const CoinTable = ({ data }) => {
 };
 
 export default CoinTable;
-
-
-
-
-
-
-
-
-
-
-
